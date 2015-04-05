3f55161821c0b3f12cc95d1c20800b60540c418a
Christian Fobel <christian@fobel.net>
2015-04-05 10:10:10 -0400

    Add install requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import numpy as np
 from path_helpers import path
 
 
-pyx_files = ['cyplace_experiments/data/CONNECTIONS_TABLE.pyx']
+pyx_files = ['fpga_netlist/CONNECTIONS_TABLE.pyx']
 ext_modules = [Extension(f[:-4].replace('/', '.'), [f],
                          extra_compile_args=['-O3', '-msse3', '-std=c++0x'],
                          include_dirs=[path('~/local/include').expand(),
@@ -29,9 +29,12 @@ setup(name="fpga_netlist",
       version=version.getVersion(),
       description="FPGA netlist models and utilities.",
       keywords="fpga netlist python",
-      author="Christian Fobel <christian@fobel.net>",
+      author="Christian Fobel",
+      author_email="<christian@fobel.net>",
       url="https://github.com/cfobel/fpga_netlist",
       license="GPL",
       long_description="""""",
       packages=['fpga_netlist'],
+      install_requires=['numpy>=1.9.0', 'Cython>=0.21', 'pandas>=0.14.1',
+                        'cythrust==0.9'],
       ext_modules=cythonize(ext_modules))
