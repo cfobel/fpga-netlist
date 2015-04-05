9f42a736ac635f99111c93886fea076872d8deb1
Christian Fobel <christian@fobel.net>
2015-04-05 10:54:44 -0400

    Compile from cpp source unless CYTHON_BUILD is set

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ import version
 
 from distutils.extension import Extension
 
-from Cython.Build import cythonize
 import numpy as np
 
 
 pyx_files = ['fpga_netlist/CONNECTIONS_TABLE.pyx']
+
+if os.environ.get('CYTHON_BUILD') is None:
+    pyx_files = [f.replace('.pyx', '.cpp') for f in pyx_files]
+
 ext_modules = [Extension(f[:-4].replace('/', '.'), [f],
                          extra_compile_args=['-O3', '-msse3', '-std=c++0x'],
                          include_dirs=[os.path.expanduser('~/local/include'),
@@ -24,6 +27,10 @@ ext_modules = [Extension(f[:-4].replace('/', '.'), [f],
                                          'THRUST_DEVICE_SYSTEM_CPP')])
                for f in pyx_files]
 
+if os.environ.get('CYTHON_BUILD') is not None:
+    from Cython.Build import cythonize
+
+    ext_modules = cythonize(ext_modules)
 
 setup(name="fpga_netlist",
       version=version.getVersion(),
@@ -35,6 +42,7 @@ setup(name="fpga_netlist",
       license="GPL",
       long_description="""""",
       packages=['fpga_netlist'],
+      include_package_data=True,
       install_requires=['numpy>=1.9.0', 'Cython>=0.21', 'pandas>=0.14.1',
                         'cythrust==0.9'],
-      ext_modules=cythonize(ext_modules))
+      ext_modules=ext_modules)
